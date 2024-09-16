6f0c4216e9566f4dc899fcfc323338eab27b5730
DilyanSvetiev <dsvetiev@gmail.com>
2024-09-16 17:24:22 +0300

    Initial commit

---
+++ trade404/src/main/java/com/ds/trade404/Trade404Application.java
@@ -0,0 +1,13 @@
+package com.ds.trade404;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Trade404Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Trade404Application.class, args);
+	}
+
+}
---
+++ trade404/src/test/java/com/ds/trade404/Trade404ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ds.trade404;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Trade404ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
