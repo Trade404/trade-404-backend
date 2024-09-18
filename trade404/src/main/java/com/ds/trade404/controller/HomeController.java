fb296611b425d48e0be3b64286619bfca196b060
DilyanSvetiev <dsvetiev@gmail.com>
2024-09-18 19:02:27 +0300

    add: HomeController class and controller package

---
+++ trade404/src/main/java/com/ds/trade404/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.ds.trade404.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping
+    public String home() {
+        return "welcome to Trade404";
+    }
+}
