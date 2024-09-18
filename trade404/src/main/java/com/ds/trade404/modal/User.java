3c213652375097670e32ddc9058d1336e1c454da
DilyanSvetiev <dsvetiev@gmail.com>
2024-09-18 19:18:19 +0300

    add: User class containing fields id, fullName, email, password and role

---
+++ trade404/src/main/java/com/ds/trade404/modal/User.java
@@ -0,0 +1,25 @@
+package com.ds.trade404.modal;
+
+import com.ds.trade404.domain.USER_ROLE;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String fullName;
+    private String email;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+
+    private USER_ROLE role = USER_ROLE.ROLE_CUSTOMER;
+}
