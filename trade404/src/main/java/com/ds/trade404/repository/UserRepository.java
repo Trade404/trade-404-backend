2e4a7ae1eb33c2847cacdb784081ca9ce62bb13f
DilyanSvetiev <dsvetiev@gmail.com>
2024-09-19 18:31:31 +0300

    add: UserRepository added for future implementation

---
+++ trade404/src/main/java/com/ds/trade404/repository/UserRepository.java
@@ -0,0 +1,6 @@
+package com.ds.trade404.repository;
+import com.ds.trade404.modal.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
