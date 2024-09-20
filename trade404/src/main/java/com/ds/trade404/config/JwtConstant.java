f9cd4d76ac4d63a9beb63bd900916589d3ab65bc
DilyanSvetiev <dsvetiev@gmail.com>
2024-09-20 16:59:49 +0300

    add: JwtConstant class with SECRET_KEY and JWT_HEADER fields

---
+++ trade404/src/main/java/com/ds/trade404/config/JwtConstant.java
@@ -0,0 +1,6 @@
+package com.ds.trade404.config;
+
+public class JwtConstant {
+    public static final String SECRET_KEY = "njhsjnigfdnhigsnijhnjosd";
+    public static final String JWT_HEADER = "Authorization";
+}
