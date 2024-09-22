b834b06f22cbbc102ba35ab0e00308e3ca875198
DilyanSvetiev <dsvetiev@gmail.com>
2024-09-22 20:54:39 +0300

    add: enhance AuthController with JWT authentication and login functionality

--- trade404/src/main/java/com/ds/trade404/controller/AuthController.java
+++ trade404/src/main/java/com/ds/trade404/controller/AuthController.java
@@ -1,10 +1,18 @@
 package com.ds.trade404.controller;
 
+import com.ds.trade404.config.JwtProvider;
 import com.ds.trade404.modal.User;
 import com.ds.trade404.repository.UserRepository;
+import com.ds.trade404.response.AuthResponse;
+import com.ds.trade404.service.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,8 +25,11 @@ public class AuthController {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
+
     @PostMapping("/signup")
-    public ResponseEntity<User> register(@RequestBody User user) throws Exception {
+    public ResponseEntity<AuthResponse> register(@RequestBody User user) throws Exception {
 
         User isEmailExisting = userRepository.findByEmail(user.getEmail());
 
@@ -33,7 +44,55 @@ public class AuthController {
 
         User savedUser = userRepository.save(newUser);
 
-        return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
+        Authentication auth = new UsernamePasswordAuthenticationToken(
+                user.getEmail(),
+                user.getPassword()
 
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(auth);
+
+        String jwt = JwtProvider.generateToken(auth);
+
+        AuthResponse res = new AuthResponse();
+        res.setJwt(jwt);
+        res.setStatus(true);
+        res.setMessage("register successful");
+
+        return new ResponseEntity<>(res, HttpStatus.CREATED);
+
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<AuthResponse> login(@RequestBody User user) throws Exception {
+
+        String userName = user.getEmail();
+        String password = user.getPassword();
+
+        Authentication auth = authenticate(userName, password);
+
+        SecurityContextHolder.getContext().setAuthentication(auth);
+
+        String jwt = JwtProvider.generateToken(auth);
+
+        AuthResponse res = new AuthResponse();
+        res.setJwt(jwt);
+        res.setStatus(true);
+        res.setMessage("login successful");
+
+        return new ResponseEntity<>(res, HttpStatus.CREATED);
+
+    }
+
+    private Authentication authenticate(String userName, String password) {
+        UserDetails userDetails = customUserDetailsService.loadUserByUsername(userName);
+
+        if(userDetails == null) {
+            throw new BadCredentialsException("invalid username");
+        }
+        if(password.equals(userDetails.getPassword())) {
+            throw new BadCredentialsException("invalid password");
+        }
+        return new UsernamePasswordAuthenticationToken(userDetails, password, userDetails.getAuthorities());
     }
 }
