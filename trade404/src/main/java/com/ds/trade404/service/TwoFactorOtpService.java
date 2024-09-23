5153e6bab6118db6ce0e5fc68dbce1e6a1433d4c
DilyanSvetiev <dsvetiev@gmail.com>
2024-09-23 18:37:47 +0300

    add: TwoFactorOtpService interface with methods for future implementation

---
+++ trade404/src/main/java/com/ds/trade404/service/TwoFactorOtpService.java
@@ -0,0 +1,17 @@
+package com.ds.trade404.service;
+
+import com.ds.trade404.modal.TwoFactorOTP;
+import com.ds.trade404.modal.User;
+
+public interface TwoFactorOtpService {
+
+    TwoFactorOTP createTwoFactorOtp(User user, String otp, String jwt);
+
+    TwoFactorOTP findByUser(Long userId);
+
+    TwoFactorOTP findById(String id);
+
+    boolean verifyTwoFactorOtp(TwoFactorOTP twoFactorOTP, String otp);
+
+    void deleteTwoFactorOtp(TwoFactorOTP twoFactorOTP);
+}
