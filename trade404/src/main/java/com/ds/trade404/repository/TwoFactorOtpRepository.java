c769220c17570fdc8a7a9eaa3bee1287a0520a21
DilyanSvetiev <dsvetiev@gmail.com>
2024-09-23 19:03:04 +0300

    add: TwoFactorOtpRepository for managing TwoFactorOTP entities

---
+++ trade404/src/main/java/com/ds/trade404/repository/TwoFactorOtpRepository.java
@@ -0,0 +1,8 @@
+package com.ds.trade404.repository;
+
+import com.ds.trade404.modal.TwoFactorOTP;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TwoFactorOtpRepository extends JpaRepository<TwoFactorOTP, String> {
+    TwoFactorOTP findByUserId(Long userId);
+}
