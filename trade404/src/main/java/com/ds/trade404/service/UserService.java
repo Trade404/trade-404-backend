d05ef159e5f8c641fd1131132073680173a4c9d8
DilyanSvetiev <dsvetiev@gmail.com>
2024-09-25 16:59:35 +0300

    add: UserServiceImpl class implementing UserService interface methods

--- trade404/src/main/java/com/ds/trade404/service/UserService.java
+++ trade404/src/main/java/com/ds/trade404/service/UserService.java
@@ -1,14 +1,17 @@
 package com.ds.trade404.service;
 
+import com.ds.trade404.domain.VerificationType;
 import com.ds.trade404.modal.User;
 
 public interface UserService {
 
-    public User findUserProfileByJwt(String jwt);
-    public User findUserByEmail(String email);
-    public User findUserById(Long userId);
+    public User findUserProfileByJwt(String jwt) throws Exception;
+    public User findUserByEmail(String email) throws Exception;
+    public User findUserById(Long userId) throws Exception;
 
-    public User enableTwoFactorAuthentication(User user);
+    public User enableTwoFactorAuthentication(VerificationType verificationType,
+                                              String sendTo,
+                                              User user);
 
     User updatePassword(User user, String newPassword);
 
