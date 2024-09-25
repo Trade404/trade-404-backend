6101bd427ebc265e5cf1020a7987e70d0de41cb7
DilyanSvetiev <dsvetiev@gmail.com>
2024-09-25 17:31:19 +0300

    add: VerificationCode entity with OTP, user association, email, mobile, and verification type

---
+++ trade404/src/main/java/com/ds/trade404/modal/VerificationCode.java
@@ -0,0 +1,25 @@
+package com.ds.trade404.modal;
+
+import com.ds.trade404.domain.VerificationType;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class VerificationCode {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String otp;
+
+    @OneToOne
+    private User user;
+
+    private String email;
+
+    private String mobile;
+
+    private VerificationType verificationType;
+}
