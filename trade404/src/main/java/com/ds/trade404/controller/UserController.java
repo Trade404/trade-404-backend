93a094469ecdf8efb66c0ce15a5369c106f4d3a7
DilyanSvetiev <dsvetiev@gmail.com>
2024-09-25 17:26:52 +0300

    add: UserController with endpoints for user profile, OTP verification, and two-factor authentication

--- trade404/src/main/java/com/ds/trade404/controller/UserController.java
+++ trade404/src/main/java/com/ds/trade404/controller/UserController.java
@@ -1,4 +1,47 @@
 package com.ds.trade404.controller;
 
+import com.ds.trade404.domain.VerificationType;
+import com.ds.trade404.modal.User;
+import com.ds.trade404.service.EmailService;
+import com.ds.trade404.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private EmailService emailService;
+    private String jwt;
+
+    @GetMapping("/api/users/profile")
+    public ResponseEntity<User> getUserProfile(@RequestHeader("Authorization") String jwt) throws Exception {
+        User user = userService.findUserProfileByJwt(jwt);
+
+        return new ResponseEntity<User>(user, HttpStatus.OK);
+    }
+
+    @PostMapping("/api/users/verification/{verificationType}/send-otp")
+    public ResponseEntity<User> sendVerificationOtp(
+            @RequestHeader("Authorization") String jwt,
+            @PathVariable VerificationType verificationType) throws Exception {
+
+        User user = userService.findUserProfileByJwt(jwt);
+
+        return new ResponseEntity<User>(user, HttpStatus.OK);
+    }
+
+    @PatchMapping("/api/users/enable-two-factor/verify-otp/{otp}")
+    public ResponseEntity<User> enableTwoFactorAuthentication(@RequestHeader("Authorization") String jwt) throws Exception {
+        User user = userService.findUserProfileByJwt(jwt);
+
+        return new ResponseEntity<User>(user, HttpStatus.OK);
+    }
+
+
 }
