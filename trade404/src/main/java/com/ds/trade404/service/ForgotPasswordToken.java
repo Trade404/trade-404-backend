7510b60c474ec45bea5c008bd7fb50651f34ba17
DilyanSvetiev <dsvetiev@gmail.com>
2024-09-27 15:28:20 +0300

    add: ForgotPasswordToken class with id, otp, verificationType and sendTo fields

---
+++ trade404/src/main/java/com/ds/trade404/service/ForgotPasswordToken.java
@@ -0,0 +1,24 @@
+package com.ds.trade404.service;
+
+import com.ds.trade404.domain.VerificationType;
+import com.ds.trade404.modal.User;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class ForgotPasswordToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @OneToOne
+    private User user;
+
+    private String otp;
+
+    private VerificationType verificationType;
+
+    private String sendTo;
+}
