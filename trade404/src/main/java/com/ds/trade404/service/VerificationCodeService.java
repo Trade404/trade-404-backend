e2786a96f48f48fed97786b208beb2d7375598bd
DilyanSvetiev <dsvetiev@gmail.com>
2024-09-27 14:25:51 +0300

    add: all functionality methods in VerificationCodeService and start the initial implementation

--- trade404/src/main/java/com/ds/trade404/service/VerificationCodeService.java
+++ trade404/src/main/java/com/ds/trade404/service/VerificationCodeService.java
@@ -1,4 +1,16 @@
 package com.ds.trade404.service;
 
+import com.ds.trade404.modal.VerificationCode;
+
 public interface VerificationCodeService {
+
+    VerificationCode sendVerificationCode(VerificationCode verificationCode);
+
+    VerificationCode getVerificationCodeById(Long id);
+
+    VerificationCode getVerificationCodeByUser(Long userId);
+
+    void deleteVerificationCodeById(VerificationCode verificationCode);
+
+
 }
--- trade404/src/main/java/com/ds/trade404/service/VerificationCodeServiceImpl.java
+++ trade404/src/main/java/com/ds/trade404/service/VerificationCodeServiceImpl.java
@@ -1,4 +1,25 @@
 package com.ds.trade404.service;
 
+import com.ds.trade404.modal.VerificationCode;
+
 public class VerificationCodeServiceImpl  implements VerificationCodeService {
+    @Override
+    public VerificationCode sendVerificationCode(VerificationCode verificationCode) {
+        return null;
+    }
+
+    @Override
+    public VerificationCode getVerificationCodeById(Long id) {
+        return null;
+    }
+
+    @Override
+    public VerificationCode getVerificationCodeByUser(Long userId) {
+        return null;
+    }
+
+    @Override
+    public void deleteVerificationCodeById(VerificationCode verificationCode) {
+
+    }
 }
