05a4569ef625931d241ab746dd8ba48241c2f439
DilyanSvetiev <dsvetiev@gmail.com>
2024-09-28 16:32:45 +0300

    add: ForgotPasswordRepository interface with findByUserId method

---
+++ trade404/src/main/java/com/ds/trade404/repository/ForgotPasswordRepository.java
@@ -0,0 +1,11 @@
+package com.ds.trade404.repository;
+
+import com.ds.trade404.service.ForgotPasswordToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ForgotPasswordRepository extends JpaRepository<ForgotPasswordToken, String> {
+
+    ForgotPasswordToken findByUserId(Long userId);
+
+
+}
