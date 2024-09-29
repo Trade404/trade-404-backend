cdf35d8e31addbf9a056a43d53118b116d91af33
DilyanSvetiev <dsvetiev@gmail.com>
2024-09-29 21:29:53 +0300

    add: CoinRepository interface

---
+++ trade404/src/main/java/com/ds/trade404/repository/CoinRepository.java
@@ -0,0 +1,9 @@
+package com.ds.trade404.repository;
+
+import com.ds.trade404.modal.Coin;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CoinRepository extends JpaRepository<Coin, Integer> {
+
+
+}
