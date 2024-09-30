538158b7efa06ab68735ea4d06380759940ab221
DilyanSvetiev <dsvetiev@gmail.com>
2024-09-30 17:54:05 +0300

    add: fully implemented getMarketChart and getCoinDetails methods in CoinServiceImpl and added throws signature

--- trade404/src/main/java/com/ds/trade404/service/CoinService.java
+++ trade404/src/main/java/com/ds/trade404/service/CoinService.java
@@ -8,9 +8,9 @@ public interface CoinService {
 
     List<Coin> getCoinList(int page) throws Exception;
 
-    String getMarketChart(String coinId, int Days);
+    String getMarketChart(String coinId, int days) throws Exception;
 
-    String getCoinDetails(String coinId);
+    String getCoinDetails(String coinId) throws Exception;
 
     Coin findById(String coinId);
 
