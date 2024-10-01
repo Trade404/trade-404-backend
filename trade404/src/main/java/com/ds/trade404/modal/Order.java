e14fc7e125263dd46fec517e44f518510b5894fc
DilyanSvetiev <dsvetiev@gmail.com>
2024-10-01 17:13:07 +0300

    add: Order class with id field

---
+++ trade404/src/main/java/com/ds/trade404/modal/Order.java
@@ -0,0 +1,17 @@
+package com.ds.trade404.modal;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+}
