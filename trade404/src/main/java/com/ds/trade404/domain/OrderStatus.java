1bc9a0ed069dee10150e8e73aeb2632d8e514557
DilyanSvetiev <dsvetiev@gmail.com>
2024-10-01 17:41:09 +0300

    add: OrderStatus and OrdedType enums

---
+++ trade404/src/main/java/com/ds/trade404/domain/OrderStatus.java
@@ -0,0 +1,6 @@
+package com.ds.trade404.domain;
+
+public enum OrderStatus {
+
+    PENDING, FILLED, CANCELED, PARTIALLY_FILLED, ERROR, SUCCESS
+}
---
+++ trade404/src/main/java/com/ds/trade404/domain/OrderType.java
@@ -0,0 +1,7 @@
+package com.ds.trade404.domain;
+
+public enum OrderType {
+
+    BUY,
+    SELL
+}
