ede8b39c11d35bcdb9ad2390caccb4cf0970f442
DilyanSvetiev <dsvetiev@gmail.com>
2024-10-02 19:30:19 +0300

    add: payOrderPayment and walletToWalletTransfer methods with endpoints

--- trade404/src/main/java/com/ds/trade404/controller/WalletController.java
+++ trade404/src/main/java/com/ds/trade404/controller/WalletController.java
@@ -1,16 +1,15 @@
 package com.ds.trade404.controller;
 
+import com.ds.trade404.modal.Order;
 import com.ds.trade404.modal.User;
 import com.ds.trade404.modal.Wallet;
+import com.ds.trade404.modal.WalletTransaction;
 import com.ds.trade404.service.UserService;
 import com.ds.trade404.service.WalletService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/wallet")
@@ -31,6 +30,32 @@ public class WalletController {
         return new ResponseEntity<>(wallet, HttpStatus.ACCEPTED);
     }
 
+    @PutMapping("/api/wallet/{walletId}/transfer")
+    public ResponseEntity<Wallet> walletToWalletTransfer(
+            @RequestHeader("Authorization") String jwt,
+            @PathVariable Long walletId,
+            @RequestBody WalletTransaction request
+            ) throws Exception {
+        User senderUser = userService.findUserProfileByJwt(jwt);
+        Wallet receiverWallet = walletService.findWalletById(walletId);
+        Wallet wallet = walletService.walletToWalletTransfer(senderUser,
+                receiverWallet,
+                request.getAmount());
+        return new ResponseEntity<>(wallet, HttpStatus.ACCEPTED);
+    }
 
+    @PutMapping("/api/wallet/order/{orderId}/pay")
+    public ResponseEntity<Wallet> payOrderPayment(
+            @RequestHeader("Authorization") String jwt,
+            @PathVariable Long orderId
+    ) throws Exception {
+        User user = userService.findUserProfileByJwt(jwt);
+
+        Order order = orderService.getOrderById(orderId);
+
+        Wallet wallet = walletService.payOrderPayment(order, user);
+
+        return new ResponseEntity<>(wallet, HttpStatus.ACCEPTED);
+    }
 
 }
