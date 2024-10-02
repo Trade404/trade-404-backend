7bfa4ee7f6b04fb37582c5958baae97b4131ff91
DilyanSvetiev <dsvetiev@gmail.com>
2024-10-02 19:55:52 +0300

    add: OrderServiceImple having partial complete implementation of OrderService methods

---
+++ trade404/src/main/java/com/ds/trade404/service/OrderServiceImpl.java
@@ -0,0 +1,73 @@
+package com.ds.trade404.service;
+
+import com.ds.trade404.domain.OrderStatus;
+import com.ds.trade404.domain.OrderType;
+import com.ds.trade404.modal.Coin;
+import com.ds.trade404.modal.Order;
+import com.ds.trade404.modal.OrderItem;
+import com.ds.trade404.modal.User;
+import com.ds.trade404.repository.OrderItemRepository;
+import com.ds.trade404.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+public class OrderServiceImpl implements OrderService{
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Autowired
+    private WalletService walletService;
+
+    @Autowired
+    private OrderItemRepository orderItemRepository;
+
+    @Override
+    public Order createOrder(User user, OrderItem orderItem, OrderType orderType) {
+
+        double price = orderItem.getCoin().getCurrentPrice()*orderItem.getQuantity();
+
+        Order order = new Order();
+        order.setUser(user);
+        order.setOrderItem(orderItem);
+        order.setOrderType(orderType);
+        order.setPrice(BigDecimal.valueOf(price));
+        order.setTimestamp(LocalDateTime.now());
+        order.setStatus(OrderStatus.PENDING);
+
+        return orderRepository.save(order);
+    }
+
+    @Override
+    public Order getOrderById(Long orderId) throws Exception {
+        return orderRepository.findById(orderId)
+                .orElseThrow(
+                        () -> new Exception("order not found"));
+    }
+
+    @Override
+    public List<Order> getAllOrdersOfUser(Long userId, OrderType orderType, String assetSymbol) {
+        return orderRepository.findByUserId(userId);
+    }
+
+
+    private OrderItem createOrderItem(Coin coin, double quantity,
+                                      double buyPrice, double sellPrice) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setCoin(coin);
+        orderItem.setQuantity(quantity);
+        orderItem.setBuyPrice(buyPrice);
+        orderItem.setSellPrice(sellPrice);
+
+        return orderItemRepository.save(orderItem);
+    }
+    @Override
+    public Order processOrder(Coin coin, double quantity, OrderType orderType, User user) {
+        return null;
+    }
+}
