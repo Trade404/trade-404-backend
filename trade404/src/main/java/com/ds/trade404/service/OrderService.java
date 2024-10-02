bf26af1a32cf693f1dc78762a3f327f7a73eb0db
DilyanSvetiev <dsvetiev@gmail.com>
2024-10-02 19:54:35 +0300

    add: OrderService interface with methods for implementation

---
+++ trade404/src/main/java/com/ds/trade404/service/OrderService.java
@@ -0,0 +1,22 @@
+package com.ds.trade404.service;
+
+import com.ds.trade404.domain.OrderType;
+import com.ds.trade404.modal.Coin;
+import com.ds.trade404.modal.Order;
+import com.ds.trade404.modal.OrderItem;
+import com.ds.trade404.modal.User;
+
+import java.util.List;
+
+public interface OrderService {
+
+    Order createOrder(User user, OrderItem orderItem, OrderType orderType);
+
+    Order getOrderById(Long orderId) throws Exception;
+
+    List<Order> getAllOrdersOfUser(Long userId, OrderType orderType, String assetSymbol);
+
+    Order processOrder(Coin coin, double quantity, OrderType orderType, User user);
+
+
+}
