578e5901fd36791958b492c33feab40c0b325aad
DilyanSvetiev <dsvetiev@gmail.com>
2024-10-02 19:53:54 +0300

    add: OrderRepository and OrderItemRepository interfaces

---
+++ trade404/src/main/java/com/ds/trade404/repository/OrderItemRepository.java
@@ -0,0 +1,9 @@
+package com.ds.trade404.repository;
+
+import com.ds.trade404.modal.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+
+
+}
---
+++ trade404/src/main/java/com/ds/trade404/repository/OrderRepository.java
@@ -0,0 +1,11 @@
+package com.ds.trade404.repository;
+
+import com.ds.trade404.modal.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    List<Order> findByUserId(Long userId);
+}
