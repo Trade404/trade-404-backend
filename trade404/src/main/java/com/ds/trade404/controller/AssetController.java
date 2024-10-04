f8762430abe692056baa3668d05a64711684608c
DilyanSvetiev <dsvetiev@gmail.com>
2024-10-04 17:02:54 +0300

    add: AssetController class with methods and endpoints for asset operations

---
+++ trade404/src/main/java/com/ds/trade404/controller/AssetController.java
@@ -0,0 +1,49 @@
+package com.ds.trade404.controller;
+
+import com.ds.trade404.modal.Asset;
+import com.ds.trade404.modal.User;
+import com.ds.trade404.service.AssetService;
+import com.ds.trade404.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/asset")
+public class AssetController {
+
+    @Autowired
+    private AssetService assetService;
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/{assetId}")
+    public ResponseEntity<Asset> getAssetById(@PathVariable Long assetId) throws Exception {
+        Asset asset = assetService.getAssetById(assetId);
+        return ResponseEntity.ok().body(asset);
+    }
+
+    @GetMapping("/coin/{coinId}/user")
+    public ResponseEntity<Asset> getUserByUserIdAndCoinId(
+            @PathVariable String coinId,
+            @RequestHeader("Authorization") String jwt
+    ) throws Exception {
+
+        User user = userService.findUserProfileByJwt(jwt);
+        Asset asset = assetService.findAssetByUserIdAndCoinId(user.getId(), coinId);
+        return ResponseEntity.ok().body(asset);
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<Asset>> getAssetForUser(
+            @RequestHeader("Authorization") String jwt
+    ) throws Exception {
+        User user = userService.findUserProfileByJwt(jwt);
+        List<Asset> assets = assetService.getUsersAssets(user.getId());
+
+        return ResponseEntity.ok().body(assets);
+    }
+}
