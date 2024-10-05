705a17788d80b939a7828f8c32857760fb322b44
DilyanSvetiev <dsvetiev@gmail.com>
2024-10-05 14:05:53 +0300

    add: Withdrawal class with id status amount user and date fields

---
+++ trade404/src/main/java/com/ds/trade404/modal/Withdrawal.java
@@ -0,0 +1,25 @@
+package com.ds.trade404.modal;
+
+import com.ds.trade404.domain.WithdrawalStatus;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+public class Withdrawal {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private WithdrawalStatus status;
+
+    private Long amount;
+
+    @ManyToOne
+    private User user;
+
+    private LocalDateTime date = LocalDateTime.now();
+}
