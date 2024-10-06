d4dabc6aec0729f16f220822151f77246984f4d1
DilyanSvetiev <dsvetiev@gmail.com>
2024-10-06 16:41:11 +0300

    add: WatchlistController class with endpoint and methods for watchlist operations

---
+++ trade404/src/main/java/com/ds/trade404/controller/WatchlistController.java
@@ -0,0 +1,55 @@
+package com.ds.trade404.controller;
+
+import com.ds.trade404.modal.Coin;
+import com.ds.trade404.modal.User;
+import com.ds.trade404.modal.Watchlist;
+import com.ds.trade404.service.CoinService;
+import com.ds.trade404.service.UserService;
+import com.ds.trade404.service.WatchlistService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/watchlist")
+public class WatchlistController {
+
+    @Autowired
+    private WatchlistService watchlistService;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private CoinService coinService;
+
+    @GetMapping("/user")
+    public ResponseEntity<Watchlist> getUserWatchlist(
+            @RequestHeader("Authorization") String jwt) throws Exception {
+
+        User user = userService.findUserProfileByJwt(jwt);
+        Watchlist watchlist = watchlistService.findUserWatchlist(user.getId());
+        return ResponseEntity.ok(watchlist);
+
+    }
+
+    @GetMapping("{watchlistId}")
+    public ResponseEntity<Watchlist> getWatchlistById(
+            @PathVariable Long watchlistId) throws Exception {
+
+        Watchlist watchlist = watchlistService.findById(watchlistId);
+        return ResponseEntity.ok(watchlist);
+    }
+
+    @PatchMapping("/add/coin/{coinId}")
+    public ResponseEntity<Coin> addItemToWatchlist(
+            @RequestHeader("Authorization") String jwt,
+            @PathVariable String coinId) throws Exception {
+
+        User user = userService.findUserProfileByJwt(jwt);
+        Coin coin = coinService.findById(coinId);
+        Coin addedCoin = watchlistService.addItemToWatchlist(coin, user);
+
+        return ResponseEntity.ok(addedCoin);
+    }
+}
