ef65671c17c7a2024bbc0f68036bc81e7faf3357
DilyanSvetiev <dsvetiev@gmail.com>
2024-10-06 16:59:23 +0300

    add: PaymentDetailsRepository interface and PaymentDetailsServiceImpl class implementing PaymentDetailsService interface methods

---
+++ trade404/src/main/java/com/ds/trade404/repository/PaymentDetailsRepository.java
@@ -0,0 +1,9 @@
+package com.ds.trade404.repository;
+
+import com.ds.trade404.modal.PaymentDetails;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PaymentDetailsRepository extends JpaRepository<PaymentDetails, Long> {
+
+    PaymentDetails findByUserId(Long userId);
+}
