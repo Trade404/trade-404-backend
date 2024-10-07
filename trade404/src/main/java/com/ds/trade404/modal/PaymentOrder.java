9a0f8adeea2e268adf8cb13293375cd23a26f9a9
DilyanSvetiev <dsvetiev@gmail.com>
2024-10-07 16:26:19 +0300

    add: PaymentOrder class with id, amount, status, paymentMethod and user fields

---
+++ trade404/src/main/java/com/ds/trade404/modal/PaymentOrder.java
@@ -0,0 +1,24 @@
+package com.ds.trade404.modal;
+
+import com.ds.trade404.domain.PaymentMethod;
+import com.ds.trade404.domain.PaymentOrderStatus;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class PaymentOrder {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private Long amount;
+
+    private PaymentOrderStatus status;
+
+    private PaymentMethod paymentMethod;
+
+    @ManyToOne
+    private User user;
+}
