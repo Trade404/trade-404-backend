44e8218574ac0e3116e29310ad4c1da2aa6f3510
DilyanSvetiev <dsvetiev@gmail.com>
2024-10-08 16:19:14 +0300

    add: PaymentController with endpoin and method for handling payments

---
+++ trade404/src/main/java/com/ds/trade404/controller/PaymentController.java
@@ -0,0 +1,45 @@
+package com.ds.trade404.controller;
+
+import com.ds.trade404.domain.PaymentMethod;
+import com.ds.trade404.modal.PaymentOrder;
+import com.ds.trade404.modal.User;
+import com.ds.trade404.response.PaymentResponse;
+import com.ds.trade404.service.PaymentService;
+import com.ds.trade404.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class PaymentController {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private PaymentService paymentService;
+
+    @PostMapping("/api/payment/{paymentMethod}/amount/{amount}")
+    public ResponseEntity<PaymentResponse> paymentHandler(
+            @PathVariable PaymentMethod paymentMethod,
+            @PathVariable Long amount,
+            @RequestHeader("Authorization") String jwt) throws Exception {
+
+        User user = userService.findUserProfileByJwt(jwt);
+
+        PaymentResponse paymentResponse;
+
+        PaymentOrder order = paymentService.createOrder(user, amount, paymentMethod);
+
+        if(paymentMethod.equals(PaymentMethod.RAZORPAY)) {
+            paymentResponse = paymentService.createRazorpayPaymentLink(user, amount);
+        } else {
+            paymentResponse = paymentService.createStripePaymentLink(user, amount, order.getId());
+        }
+
+        return new ResponseEntity<>(paymentResponse, HttpStatus.CREATED);
+    }
+
+}
